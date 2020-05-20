546b09e739b3a670f28582a3647b4abbca3dad5a
pablo-guerra <pabloguerra.can@gmail.com>
2020-05-20 21:05:29 +0200

    Cleaned main.js

--- static/main.js
+++ static/main.js
@@ -1,18 +1,7 @@
-//Dummy dictionary data
-
-//const data = [
-//  ['eat', 'ate', 'eaten', 'comer'],
-//  ['fly', 'flew', 'flown', 'volar'],
-//  ['jump', 'jumped', 'jumped', 'saltar'],
-//  ['set', 'set', 'set', 'montar']
-//];
-
-
 //initialize verb index variables
 console.log(data);
 const verb_total_length = Object.keys(data).length;
 let current_verb_index = verb_total_length-1;
-
 console.log(`Number of verbs loaded = ${verb_total_length}`);
 console.log('Inf number 1 = ' + data[current_verb_index][0]);
 //function to update the displayed text
@@ -22,7 +11,6 @@ function updateDisplayedVerbs(verb_index = current_verb_index) {
   document.getElementById("text_per").innerHTML = data[current_verb_index][2];
   document.getElementById("text_eng").innerHTML = data[current_verb_index][3];
 };
-// updateDisplayedVerbs(); // update the displayed verbs
 // initialize slider variable
 let current_slider_state = false;
 function getRandomInt(min = 0, max = (verb_total_length - 1)) {
@@ -43,7 +31,6 @@ function hideTiles() {
   document.getElementById("imperfectum").className = "invisible";
   document.getElementById("perfectum").className = "invisible";
 };
-
 //slider switch
 document.getElementById("mode-switch").addEventListener("click", function(){
   if(current_slider_state === false) {
@@ -53,7 +40,6 @@ document.getElementById("mode-switch").addEventListener("click", function(){
     current_slider_state = false;
     document.getElementById("app-mode-text").innerHTML = 'Alfabetisch';
   };});
-
 //Switch words back and forth:
 document.getElementById("go-left").addEventListener("click", function(){
   console.log(`Current verb index is = ${current_verb_index}`);
@@ -85,8 +71,6 @@ document.getElementById("go-right").addEventListener("click", function(){
   console.log(`New verb index is = ${current_verb_index}`);
   hideTiles();
 });
-
-
 //Switch visibility of English translation
 document.getElementById("switch-english").addEventListener("click", function(){
   if(btn_english_state === false) {
@@ -96,26 +80,23 @@ document.getElementById("switch-english").addEventListener("click", function(){
     console.log(`NEW english button state = ${btn_english_state}`);
   } else {
     console.log(`[click] English button state is = ${btn_english_state}`);
-
     btn_english_state = false;
     document.getElementById("english-translation").className = "invisible";
     console.log(`NEW english button state = ${btn_english_state}`);
   };});
+//Switch visibility of conjugation
+document.getElementById("switch-conjugation").addEventListener("click", function(){
+  if(btn_conjugate_state === false) {
+    console.log(`[click] conjugation button state is = ${btn_conjugate_state}`);
+    btn_conjugate_state = true;
+    document.getElementById("imperfectum").className = "visible";
+    document.getElementById("perfectum").className = "visible";
+    console.log(`NEW conjugation button state = ${btn_conjugate_state}`);
+  } else {
+    console.log(`[click] conjugation button state is = ${btn_conjugate_state}`);
 
-
-  //Switch visibility of conjugation
-  document.getElementById("switch-conjugation").addEventListener("click", function(){
-    if(btn_conjugate_state === false) {
-      console.log(`[click] conjugation button state is = ${btn_conjugate_state}`);
-      btn_conjugate_state = true;
-      document.getElementById("imperfectum").className = "visible";
-      document.getElementById("perfectum").className = "visible";
-      console.log(`NEW conjugation button state = ${btn_conjugate_state}`);
-    } else {
-      console.log(`[click] conjugation button state is = ${btn_conjugate_state}`);
-
-      btn_conjugate_state = false;
-      document.getElementById("imperfectum").className = "invisible";
-      document.getElementById("perfectum").className = "invisible";
-      console.log(`NEW conjugation button state = ${btn_conjugate_state}`);
-    };});
+    btn_conjugate_state = false;
+    document.getElementById("imperfectum").className = "invisible";
+    document.getElementById("perfectum").className = "invisible";
+    console.log(`NEW conjugation button state = ${btn_conjugate_state}`);
+  };});
